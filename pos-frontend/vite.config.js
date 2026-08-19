8ee2e4459ddca94e5061aa02aa9025be3055ed8c
anshuman-bhol <anshumanbhol001@gmail.com>
2026-08-19 10:54:48 +0530

    ui fixes 3 for device compatibility

--- pos-frontend/vite.config.js
+++ pos-frontend/vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
         theme_color: "#000000",
         background_color: "#000000",
 
-        display: "standalone",
+        display: "fullscreen",
 
         start_url: "/",
 
