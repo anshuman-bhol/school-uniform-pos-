5e073a5e1091299db06ea83ded55d03d4b619f70
anshuman-bhol <anshumanbhol001@gmail.com>
2026-08-27 09:29:52 +0530

    access update for showing transactions

--- pos-backend/routes/orderRoute.js
+++ pos-backend/routes/orderRoute.js
@@ -1,12 +1,11 @@
 const express = require("express")
 const { addOrder, getOrders, getPaymentLedger, getOrderById, updateOrder, updateOrderPayment, changeOrderTailor, updateOrderBill} = require("../controllers/orderController")
 const { isVerifiedUser } = require("../middlewares/tokenVerification")
-const authorizeRoles = require("../middlewares/authorizeRoles");
 const router = express.Router()
 
 router.route("/").post(isVerifiedUser,addOrder)
 router.route("/").get(isVerifiedUser,getOrders)
-router.route("/payment-ledger").get(isVerifiedUser, authorizeRoles("admin"), getPaymentLedger);
+router.route("/payment-ledger").get(isVerifiedUser, getPaymentLedger);
 router.route("/:id").get(isVerifiedUser,getOrderById)
 router.route("/:id").put(isVerifiedUser,updateOrder)
 router.route("/:id/change-tailor").put(isVerifiedUser, changeOrderTailor);
